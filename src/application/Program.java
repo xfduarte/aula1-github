a5b7a82a6357b12c3255794af8f00c340cf8dc2a
Fernando Duarte <duarte.brusque@gmail.com>
2020-05-08 12:35:25 -0300

    Criando um commit

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,6 +4,10 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Git and GitHub bonus class!");
 		
+		System.out.println("Eclipse->File->Import->General->Existing projects into workspace ");
+		System.out.println("para dar commit nas mudanças primeiro joga na area de stage com o comando\n"
+				+ "git add .\nDepois commitar com o comando\n"
+				+ "git commit -m \"nome daalteração\"");
 	}
 
 }
